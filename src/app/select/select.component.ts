3fe3559e8ccf75e07915fc6caf4fdc1217501466
witbitbuck <witold.krysztofiak@globegroup.pl>
2017-03-30 16:12:42 +0200

    refactor

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input, Output,EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'ui-select',
@@ -8,15 +8,15 @@ import { Component, OnInit,Input, Output,EventEmitter } from '@angular/core';
 export class SelectComponent implements OnInit {
 
   @Input() data: any[] = [];
-  tempData: any[];
-  @Output() dataChange: any = new EventEmitter<any>();
-  showSelect: boolean;
-  startData: string | number;
   @Input() label: string = 'label';
   @Input() showPropertyName: string = 'name';
   @Input() emitPropertyName: string = 'id';
   @Input() showSearch: boolean = false;
   @Input() showLabel: boolean = false;
+  @Output() dataChange: any = new EventEmitter<any>();
+  tempData: any[];
+  showSelect: boolean;
+  startData: string | number;
   emptySelect: boolean;
 
   constructor() { }
@@ -38,13 +38,17 @@ export class SelectComponent implements OnInit {
     this.dataChange.emit(item[this.emitPropertyName]);
   }
   search(query: string | number) {
-    this.tempData = this.data.filter(item => {
-     return item[this.showPropertyName].toString().toLowerCase().indexOf(query.toString().toLowerCase()) > -1;
-    });
-    if (this.tempData.length === 0) {
-      this.startData = ' ';
-    }else{
+    this.tempData = this.data.filter(
+      item => (item[this.showPropertyName]
+              .toString()
+              .toLowerCase()
+              .indexOf(
+                query
+                .toString()
+                .toLowerCase()) > -1
+        ));
+    this.tempData.length === 0 ?
+      this.startData = '' :
       this.startData = this.tempData[0][this.showPropertyName];
-    }
   }
 }
